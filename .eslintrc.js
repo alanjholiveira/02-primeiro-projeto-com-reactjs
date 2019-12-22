4de124ab9f6feee8acb5350b3e6a182804588ee8
Alan Oliveira <alan@thinkpad-x250.localdomain>
2019-12-22 15:59:05 -0300

    Criando primeiro projeto com ReactJS

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export': 'off',
+    "react/state-in-constructor": [0, "never"],
+    "react/static-property-placement": ["warn", "property assignment", {
+      childContextTypes: "static getter",
+      contextTypes: "static public field",
+      contextType: "static public field",
+      displayName: "static public field",
+    }]
+  },
+};
